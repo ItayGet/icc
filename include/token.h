5739b2b059ead9e72e147b4deac1c1eb9ee7d054
Itay Getraide <itaygetraide@gmail.com>
2020-12-09 14:57:11 +0200

    Remove unnecessary include and macro

--- include/token.h
+++ include/token.h
@@ -1,8 +1,6 @@
 // Token
 #pragma once
 
-#include <limits.h>
-
 typedef enum {
 	tokenError,
 	tokenEOF,
@@ -11,9 +9,6 @@ typedef enum {
 	tokenStringLiteral,
 	tokenPunctuator,
 } TokenType;
-
-// Used to create punctuators that exeed the limits of chars
-#define OVERFLOW_CHAR(c) c + UCHAR_MAX + 1
 
 // Punctuator types sometimes have values that exceed the limits of chars
 typedef enum {
