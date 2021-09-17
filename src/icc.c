dcd4f17ad1bf4ce42e71ff6b585d51364b5046eb
Itay Getraide <itaygetraide@gmail.com>
2021-09-17 16:47:23 +0300

    Main function uses arguments to parse out whether to read expression from stdin, a file, or from args

--- src/icc.c
+++ src/icc.c
@@ -2,15 +2,38 @@
 #include "exprparser.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-char progStr[] = "(int) a ++ + (a?b:c)";
-
-int main() {
-	
+// Temporary system: 
+// arg "-" says to read from stdin,
+// arg "-f <file>" reads from file,
+// arg "-a <prog>" reads from args
+int main(int argc, char *argv[]) {
 	Stream s;
-	s.type = StreamArray;
-	s.array.array = progStr;
-	s.array.pointer = progStr;
+
+	if(argc == 2) {
+		if(!strcmp(argv[1], "-")) {
+			s.type = StreamFile;
+			s.file.file = stdin;
+		}
+	}
+	else if(argc == 3) {
+		if(!strcmp(argv[1], "-f")) {
+			s.type = StreamFile;
+			s.file.file = fopen(argv[2], "r");
+		} else if(!strcmp(argv[1], "-a")) {
+			s.type = StreamArray;
+			s.array.array = argv[2];
+			s.array.pointer = argv[2];
+		} else {
+			puts("Args not properly formatted");
+			exit(1);
+		}
+	} else {
+		puts("Args not properly formatted");
+		exit(1);
+	}
 		
 	TokenStream ts;
 	makeTokenStream(&ts);
